03653d456f6432628137041eea45a3e69d867fb0
Sergey Tekuchev <tsc-targitaj@yandex.ru>
2023-03-27 14:38:36 +0300

    Nginx config

---
+++ nginx/www/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title> Тестовое задание cdnnow </title>
+<style type="text/css">
+<!--
+h1	{text-align:center;
+	font-family:Arial, Helvetica, Sans-Serif;
+	}
+p	{text-ident:20px;
+	}
+-->
+</style>
+</head>
+<body bgcolor = "#ffffcc" text = "#000000">
+<h1>
+<?php
+echo "Hello there!!";
+echo "</br>";
+echo "cdnnow test";
+echo "</br>";
+echo "This is PHP!!!";
+?>
+</h1>
+</body>
+</html>
